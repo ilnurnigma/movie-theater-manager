f96fa8ead5d7a1d19b32e2beb66db52792e5ef71
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2016-07-01 13:12:00 +0200

    Starting to code.

---
+++ manager/src/main/java/my/manager/Main.java
@@ -0,0 +1,10 @@
+package my.manager;
+
+/**
+ * Created on 7/1/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ manager/src/main/java/my/manager/service/UserService.java
@@ -0,0 +1,53 @@
+package my.manager.service;
+
+import my.manager.dao.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 7/1/2016.
+ */
+public class UserService {
+    private static List<User> userList = new ArrayList<User>();
+
+    public void save(User user) {
+        userList.add(user);
+    }
+
+    public void remove(User user) {
+        userList.remove(user);
+    }
+
+    public User getById(Long id) {
+        if (id == null) {
+            return null;
+        }
+
+        for (User user : userList) {
+            if (id.equals(user.getId())) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public User getUserByEmail(String email) {
+        if (email == null) {
+            return null;
+        }
+
+        for (User user : userList) {
+            if (email.equals(user.getEmail())) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public List<User> getAll() {
+        return userList;
+    }
+}
