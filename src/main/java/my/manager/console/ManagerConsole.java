4b05fb71a72bab531ff973d1868d6b7619ade28d
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2016-10-07 10:57:04 +0200

    Created base console functionality.

--- src/main/java/my/manager/console/ManagerConsole.java
+++ src/main/java/my/manager/console/ManagerConsole.java
@@ -19,17 +19,38 @@ public class ManagerConsole {
 
     public void start() {
         System.out.println("Console started.");
+        System.out.println("1 - get all users");
+        System.out.println("2 - save user");
+        System.out.println("0 - exit");
         String line;
         do {
             line = scanner.nextLine();
 
             switch (line) {
+                case "1":
                 case "get users":
                     List<User> all = userService.getAll();
                     System.out.println(all);
+                    break;
+                case "2":
+                case "save":
+                    saveUser();
+
+                    break;
+
             }
-        } while (!"exit".equals(line));
+        } while (!"0".equals(line) || !"exit".equals(line));
 
 
+    }
+
+    private void saveUser() {
+        System.out.println("Enter user name");
+        String name = scanner.nextLine();
+        System.out.println("Enter user surname");
+        String surname = scanner.nextLine();
+        System.out.println("Enter user e-mail");
+        String email = scanner.nextLine();
+        new UserService().save(new User(123, name, surname, email));
     }
 }
